6a8139e8730d9498852746754a910548c0033d30
snovikov <snovikov@DESKTOP-AG27V9A>
2019-07-29 00:52:51 +0300

    в разобранном состоянии

--- src/main/java/nsa/chatbot/WorkWithMessages.java
+++ src/main/java/nsa/chatbot/WorkWithMessages.java
@@ -6,21 +6,57 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
+import chat.tamtam.botapi.TamTamBotAPI;
+import chat.tamtam.botapi.exceptions.APIException;
+import chat.tamtam.botapi.exceptions.ClientException;
+import chat.tamtam.botapi.model.Attachment;
+import chat.tamtam.botapi.model.AttachmentRequest;
+import chat.tamtam.botapi.model.MessageBody;
+import chat.tamtam.botapi.model.NewMessageBody;
+import chat.tamtam.botapi.model.NewMessageLink;
+import chat.tamtam.botapi.model.SendMessageResult;
+import chat.tamtam.botapi.queries.SendMessageQuery;
+
 import java.io.IOException;
+import java.util.List;
 
 public class WorkWithMessages {
 	
+	public WorkWithMessages() {
+		
+		
+		
+	}
+	
+	public static String stoken = "wRykpqkYXo0EvYqGTVaESoVcABmSZEi4PBjZ8hyOzQk";
 		
 	public static void readJson(String request) throws IOException {
 		ObjectMapper mapper = new ObjectMapper();
 		try {
         JsonNode rootNode = mapper.readValue(request, JsonNode.class);
-        String message = rootNode.get("user_id").asText();
+        JsonNode childNodeMessage =  rootNode.get("message");
+        JsonNode childNodeBody =  childNodeMessage.get("body");
+        JsonNode childNodeText = childNodeBody.get("text");
+        
+        String message = childNodeText.asText();
         System.out.println("---------------------------------");
         System.out.println(message);
 			} catch (IOException e) {
 				System.out.println(e.getMessage());	
 			}
 		}
+	
+	public static void sendAnswer(String messageToUser) throws ClientException, APIException {
+		//String mid = "mid.0000000c7282027a016c374712671b7f";
+		//long seq = 102517669913631615;
+		List<AttachmentRequest> attach = null;
+		long userId = 573535126362;
+		NewMessageLink link = null;
+		
+		TamTamBotAPI api = TamTamBotAPI.create(stoken);
+		NewMessageBody body = new NewMessageBody("hello world!", attach, link);
+		SendMessageQuery sendMessageQuery = api.sendMessage(body).userId(userId);
+		SendMessageResult result = sendMessageQuery.execute();
+	}
 
 }
